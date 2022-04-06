3c74b9a03e98c07ad95bf18a3af8fa997282442f
jonathan-pasco-arnone <jonathan.pasco-arnone@mths.ca>
2022-04-06 14:43:36 +0100

    sum 1 working

--- sum01.s
+++ sum01.s
@@ -5,7 +5,7 @@
 
 /*The data section filled with variables*/
 .data
-    string: .asciz "The number is: %d\n"
+    string: .asciz "The sum is: %d\n"
 
 /*The text section filled with code*/
 .text
@@ -16,8 +16,8 @@ main:
     push {ip, lr}
 
     ldr r0, =string
-    mov r2, #3
-    mov r1, #4
+    mov r2, #-3
+    mov r1, #2
     add r1, r1, r2
     bl printf        /*Seems to always read r1*/
 
