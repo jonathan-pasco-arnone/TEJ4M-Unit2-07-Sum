49963a5c4e4c8cb0249ee8b20c4d2cbc774b10ab
jonathan-pasco-arnone <jonathan.pasco-arnone@mths.ca>
2022-04-06 14:55:39 +0100

    still using c compiler

---
+++ sum02.s
@@ -0,0 +1,11 @@
+/* Created by: Jonathan Pasco-Arnone
+ * Created April 2022
+ * This program generates a sum
+ */
+
+.global main
+main:
+    mov r0, #3
+    mov r1, #4
+    add r0, r0, r1
+    bx  lr
